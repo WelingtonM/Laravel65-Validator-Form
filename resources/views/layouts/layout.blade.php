e26d69173671186e1452962b5224e8b80091275a
Welington Miyazato <welingtonmiyazato@gmail.com>
2019-12-15 23:02:59 +0900

    Blade - Working with Layouts

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="/sass/app.scss" />
+		<script type="text/javascript" src="/js/app.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="row">
+			<div class="container">
+        		<h1>Laravel 6.5: Validation Forms</h1>
+        		@yield('content')
+			</div>
+		</div>
+	</body>
+</html>
