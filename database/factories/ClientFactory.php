29be740172f54a34b5aa7827cf5d1d8f68d90d78
Welington Miyazato <welingtonmiyazato@gmail.com>
2019-12-15 22:13:33 +0900

    CPF

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -5,9 +5,13 @@
 use App\Client;
 use Faker\Generator as Faker;
 
+require_once __DIR__ . '/../faker_data/document_number.php';
+
 $factory->define(Client::class, function (Faker $faker) {
+    $cpfs = cpfs();
     return [
         'name' => $faker->name,
+        'document_number' => $cpfs[array_rand($cpfs, 1)],
         'email' => $faker->email,
         'phone' => $faker->phoneNumber,
         'defaulter' => rand(0,1),
@@ -17,13 +21,3 @@ $factory->define(Client::class, function (Faker $faker) {
         'physical_disability' => rand(1, 10) % 2 == 0 ? $faker->word : null
     ];
 });
-
-//     $table->string('name');
-//     $table->string('document_number');
-//     $table->string('email');
-//     $table->string('phone');
-//     $table->boolean('defaulter');
-//     $table->date('date_birth');
-//     $table->char('sex', 10);
-//     $table->enum('marital_status', array_keys(\App\Client::MARITAL_STATUS));
-//     $table->string('physical_disability')->nullable();
