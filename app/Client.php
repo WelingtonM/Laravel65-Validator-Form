0761d7c0d476fd59df946358c065962564dacc43
Welington Miyazato <welingtonmiyazato@gmail.com>
2019-12-18 20:37:38 +0900

    Creating Fields to Separate Client Types

--- app/Client.php
+++ app/Client.php
@@ -5,6 +5,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
+    const TYPE_PERSON_PHYSICAL = 'person_physical';
+    const TYPE_PERSON_COMMPANY = 'person_company';
 
     const MARITAL_STATUS = [
         1 => 'single',
